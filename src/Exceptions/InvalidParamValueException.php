0c30cd6566c8af55b34ffac8c81154e1679bc89e
railken <railken@protonmail.ch>
2017-09-20 23:03:27 +0200

    cleanup

--- src/Exceptions/InvalidParamValueException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Railken\Laravel\Manager\Exceptions;
-
-use Exception;
-
-class InvalidParamValueException extends Exception
-{
-}
--- src/Exceptions/MissingParamException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Railken\Laravel\Manager\Exceptions;
-
-use Exception;
-
-class MissingParamException extends Exception
-{
-}
