3b6747c45086a97dc308ecc9addc017171828d38
railken <railken@protonmail.ch>
2017-09-20 23:02:50 +0200

    improved exception not authorized

--- src/Exceptions/NotAuthorizedException.php
+++ src/Exceptions/NotAuthorizedException.php
@@ -6,5 +6,18 @@ use Exception;
 
 class NotAuthorizedException extends Exception
 {
-	
+
+	/**
+	 * The code to identify the error
+	 *
+	 * @var string
+	 */
+	protected $code = 'NOT_AUTHORIZED';
+
+	/**
+	 * The message
+	 *
+	 * @var string
+	 */
+	protected $message = "You're not authorized";
 }
