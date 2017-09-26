6bf43463987ef385923a3d641b86c1c4e2945246
Unknown <railken@protonmail.ch>
2017-09-26 23:05:41 +0200

    split up tests

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -2,27 +2,17 @@
 
 namespace Railken\Laravel\Manager\Tests;
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Database\Schema\Blueprint;
-
-use Railken\Laravel\Manager\Generator;
+use Illuminate\Support\Facades\Schema;
+use Railken\Bag;
+use Railken\Laravel\Manager\Agents\SystemAgent;
+use Railken\Laravel\Manager\Tests\Core\Article\ArticleManager;
+use Railken\Laravel\Manager\Tests\Core\Article\ArticleServiceProvider;
+use Railken\Laravel\Manager\Tests\Core\Comment\CommentManager;
+use Railken\Laravel\Manager\Tests\Core\Comment\CommentServiceProvider;
 use Railken\Laravel\Manager\Tests\User\User;
 use Railken\Laravel\Manager\Tests\User\UserManager;
 use Railken\Laravel\Manager\Tests\User\UserServiceProvider;
-use Railken\Bag;
-use Illuminate\Support\Facades\File;
-
-use Railken\Laravel\Manager\Tests\Generated\Foo\FooManager;
-use Railken\Laravel\Manager\Tests\Generated\Foo\FooParameterBag;
-use Railken\Laravel\Manager\Tests\Generated\Foo\FooServiceProvider;
-
-use Railken\Laravel\Manager\Tests\Core\Article\ArticleManager;
-use Railken\Laravel\Manager\Tests\Core\Comment\CommentManager;
-use Railken\Laravel\Manager\Tests\Core\Article\ArticleServiceProvider;
-use Railken\Laravel\Manager\Tests\Core\Comment\CommentServiceProvider;
-
-use Railken\Laravel\Manager\Agents\SystemAgent;
 
 class BasicTest extends \Orchestra\Testbench\TestCase
 {
@@ -72,16 +62,6 @@ class BasicTest extends \Orchestra\Testbench\TestCase
             $table->timestamps();
         });
 
-
-        Schema::create('foo', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name')->nullable();
-            $table->timestamps();
-            $table->softDeletes();
-        });
-
-
-
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
@@ -117,36 +97,6 @@ class BasicTest extends \Orchestra\Testbench\TestCase
         return new Bag(['email' => 'test@test.net', 'username' => 'test123', 'password' => microtime()]);
     }
 
-    /**
-     * Test generate Command
-     */
-    public function testGenerate()
-    {
-        $generator = new Generator();
-        $generator->generate(__DIR__."/Generated", "Railken\Laravel\Manager\Tests\Generated\Foo");
-        $this->assertEquals(true, File::exists(__DIR__."/Generated/Foo"));
-
-        (new FooServiceProvider($this->app))->register();
-
-        $m = new FooManager();
-
-        $um = new UserManager();
-        $user = $um->create($this->getUserBag())->getResource();
-
-        $bag = new FooParameterBag(['name' => 'a']);
-        $m->setAgent($user);
-        $this->assertEquals("FOO_NAME_NOT_VALID", $m->create($bag->set('name', '2'))->getError()->getCode());
-        $this->assertEquals(true, $m->create($bag->set('name', null))->ok());
-
-        $foo = $m->create($bag->set('name', null))->getResource();
-        $m->update($foo, $bag->set('name', 'fee'))->getResource();
-
-        $foo_s = $m->findOneBy(['name' => 'fee']);
-
-        $this->assertEquals($foo->id, $foo_s->id);
-
-        $um->remove($user);
-    }
 
     /**
      * Test generate Command
---
+++ tests/GeneratedTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Railken\Laravel\Manager\Tests;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Schema;
+use Railken\Bag;
+use Railken\Laravel\Manager\Generator;
+use Railken\Laravel\Manager\Tests\Generated\Foo\FooManager;
+use Railken\Laravel\Manager\Tests\Generated\Foo\FooParameterBag;
+use Railken\Laravel\Manager\Tests\Generated\Foo\FooServiceProvider;
+
+class GeneratedTest extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            \Railken\Laravel\Manager\ManagerServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Setup the test environment.
+     */
+    public function setUp()
+    {
+        $dotenv = new \Dotenv\Dotenv(__DIR__."/..", '.env');
+        $dotenv->load();
+
+        parent::setUp();
+
+        Schema::dropIfExists('foo');
+
+        Schema::create('foo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Return a new instance of user bag
+     *
+     * @return Bag
+     */
+    public function getUserBag()
+    {
+        return new Bag(['email' => 'test@test.net', 'username' => 'test123', 'password' => microtime()]);
+    }
+
+    /**
+     * Test generate Command
+     */
+    public function testGenerate()
+    {
+        $generator = new Generator();
+        $generator->generate(__DIR__."/Generated", "Railken\Laravel\Manager\Tests\Generated\Foo");
+        $this->assertEquals(true, File::exists(__DIR__."/Generated/Foo"));
+
+        (new FooServiceProvider($this->app))->register();
+
+        $m = new FooManager();
+
+        $bag = new FooParameterBag(['name' => 'a']);
+        $this->assertEquals("FOO_NAME_NOT_VALID", $m->create($bag->set('name', '2'))->getError()->getCode());
+        $this->assertEquals(true, $m->create($bag->set('name', null))->ok());
+
+        $foo = $m->create($bag->set('name', null))->getResource();
+        $m->update($foo, $bag->set('name', 'fee'))->getResource();
+
+        $foo_s = $m->findOneBy(['name' => 'fee']);
+
+        $this->assertEquals($foo->id, $foo_s->id);
+    }
+}
