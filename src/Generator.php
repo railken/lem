59def45ff72ffca27faff3124e06c4ae4122c7b4
railken <railken@protonmail.ch>
2017-09-30 16:09:50 +0200

    fix camel/underscore vars in stub

--- src/Generator.php
+++ src/Generator.php
@@ -15,6 +15,29 @@ class Generator
     {
     }
 
+    /**
+     * Camelizes a string.
+     *
+     * @param string $id A string to camelize
+     *
+     * @return string The camelized string
+     */
+    public static function camelize($id)
+    {
+        return strtr(ucwords(strtr($id, array('_' => ' ', '.' => '_ ', '\\' => '_ '))), array(' ' => ''));
+    }
+
+    /**
+     * A string to underscore.
+     *
+     * @param string $id The string to underscore
+     *
+     * @return string The underscored string
+     */
+    public static function underscore($id)
+    {
+        return strtolower(preg_replace(array('/([A-Z]+)([A-Z][a-z])/', '/([a-z\d])([A-Z])/'), array('\\1_\\2', '\\1_\\2'), str_replace('_', '.', $id)));
+    }
 
     /**
      * Generate a new ModelStructure folder
@@ -30,11 +53,12 @@ class Generator
         $name = $namespaces->last();
         $base_path = $path."/".$name;
 
+        $name = $this->camelize($name);
         $vars = [
             'NAMESPACE' => $namespace,
             'NAME' => $name,
-            'LOW:NAME' => strtolower($name),
-            'UP:NAME' => strtoupper($name),
+            'NAME:UNDERSCORE' => $this->underscore($name),
+            'NAME:UPPERCASE' => strtoupper($name),
         ];
 
         $this->base_path = $base_path;
@@ -71,28 +95,32 @@ class Generator
         $namespaces = collect(explode("\\", $namespace));
         $name = $namespaces->last();
         $base_path = $path."/".$name;
+        $name = $this->camelize($name);
 
         $attribute_ucf = ucfirst($attribute);
+
+        $attribute_camelized = $this->camelize($attribute);
+        $attribute_underscore = $this->underscore($attribute);
 
         $vars = [
             'NAMESPACE' => $namespace,
             'NAME' => $name,
-            'LOW:NAME' => strtolower($name),
-            'UP:NAME' => strtoupper($name),
-            'ATTRIBUTE:LOW' => strtolower($attribute),
-            'ATTRIBUTE:FUP' => $attribute_ucf,
-            'ATTRIBUTE:UP' => strtoupper($attribute)
+            'NAME:UNDERSCORE' => strtolower($name),
+            'NAME:UPPERCASE' => strtoupper($name),
+            'ATTRIBUTE:UNDERSCORE' => $attribute_underscore,
+            'ATTRIBUTE:CAMELIZED' => $attribute_camelized,
+            'ATTRIBUTE:UPPERCASE' => strtoupper($attribute)
         ];
 
         $this->base_path = $base_path;
 
-        $this->put("/Exceptions/ModelAttributeNotDefinedException.php.stub", "/Exceptions/{$name}".ucfirst($attribute)."NotDefinedException.php", $vars);
-        $this->put("/Exceptions/ModelAttributeNotValidException.php.stub", "/Exceptions/{$name}".ucfirst($attribute)."NotValidException.php", $vars);
+        $this->put("/Exceptions/ModelAttributeNotDefinedException.php.stub", "/Exceptions/{$name}".($attribute_camelized)."NotDefinedException.php", $vars);
+        $this->put("/Exceptions/ModelAttributeNotValidException.php.stub", "/Exceptions/{$name}".($attribute_camelized)."NotValidException.php", $vars);
         // Updating validator.
 
         // Retrieve class validator
         $c = "\\{$namespace}\\{$name}Validator";
-        if (class_exists($c) && !in_array("validate{$attribute_ucf}", get_class_methods($c))) {
+        if (class_exists($c) && !in_array("validate{$attribute_camelized}", get_class_methods($c))) {
 
             // We are free to add code
 
