6ffa544d239d18d368bdacaad993eb23f273f4fa
Unknown <railken@protonmail.ch>
2018-03-18 17:07:29 +0100

    cleanup manager service provider

--- src/ManagerServiceProvider.php
+++ src/ManagerServiceProvider.php
@@ -14,13 +14,6 @@ class ManagerServiceProvider extends ServiceProvider
     public $app;
 
     /**
-     * List of all version accepted.
-     *
-     * @var array
-     */
-    public $versions = ['5'];
-
-    /**
      * Register the service provider.
      *
      * @return void
@@ -28,30 +21,5 @@ class ManagerServiceProvider extends ServiceProvider
     public function register()
     {
         $this->commands([Commands\Generate::class, Commands\GenerateAttribute::class]);
-
-        $v = explode('.', $this->app->version());
-
-        if (!in_array($v[0], $this->versions)) {
-            throw new \Exception("Version {$this->app->version()} not supported");
-        }
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return [];
     }
 }
