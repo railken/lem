f42eb890935ea0127eb6e2be7f19af83f92ac8ca
Unknown <railken@protonmail.ch>
2018-03-26 14:53:34 +0200

    fix

--- src/Attributes/BelongsToAttribute.php
+++ src/Attributes/BelongsToAttribute.php
@@ -24,10 +24,10 @@ abstract class BelongsToAttribute extends BaseAttribute implements BelongsToAttr
     {
         $errors = new Collection();
 
-        $this->required && !$entity->exists && !$parameters->exists($this->getName()) &&
+        $this->required && !$entity->exists && !$parameters->exists($this->getRelationName()) &&
             $errors->push(new $this->exceptions[Tokens::NOT_DEFINED]($parameters->get($this->getName())));
 
-        $this->unique && $parameters->exists($this->getName()) && $this->isUnique($entity, $parameters->get($this->getName())) &&
+        $this->unique && $parameters->exists($this->getRelationName()) && $this->isUnique($entity, $parameters->get($this->getRelationName())) &&
             $errors->push(new $this->exceptions[Tokens::NOT_UNIQUE]($parameters->get($this->getName())));
 
         $parameters->exists($this->getRelationName()) && !$this->valid($entity, $parameters->get($this->getRelationName())) &&
@@ -73,6 +73,7 @@ abstract class BelongsToAttribute extends BaseAttribute implements BelongsToAttr
             $parameters->set($this->getRelationName(), $this->getDefault($entity));
         }
 
+
         $errors = $errors->merge($this->authorize(Tokens::PERMISSION_FILL, $entity, $parameters));
         $errors = $errors->merge($this->validate($entity, $parameters));
 
@@ -95,7 +96,7 @@ abstract class BelongsToAttribute extends BaseAttribute implements BelongsToAttr
     {
         $errors = new Collection();
 
-        $parameters->exists($this->getName()) && $this->getRelationBuilder($entity)->associate($parameters->get($this->getRelationName()));
+        $parameters->exists($this->getRelationName()) && $this->getRelationBuilder($entity)->associate($parameters->get($this->getRelationName()));
 
         return $errors;
     }
