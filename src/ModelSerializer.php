e5d1b4cbc42bd25fb60d4050df2622e28234857d
railken <railken@protonmail.ch>
2018-05-24 14:30:37 +0200

    update serializer

--- src/ModelSerializer.php
+++ src/ModelSerializer.php
@@ -4,6 +4,9 @@ namespace Railken\Laravel\Manager;
 
 use Railken\Laravel\Manager\Contracts\ManagerContract;
 use Railken\Laravel\Manager\Contracts\ModelSerializerContract;
+use Railken\Laravel\Manager\Contracts\EntityContract;
+use Railken\Laravel\Manager\Tokens;
+use Illuminate\Support\Collection;
 
 abstract class ModelSerializer implements ModelSerializerContract
 {
@@ -17,5 +20,26 @@ abstract class ModelSerializer implements ModelSerializerContract
     public function __construct(ManagerContract $manager = null)
     {
         $this->manager = $manager;
+    }
+
+    /**
+     * Serialize entity.
+     *
+     * @param EntityContract $entity
+     * @param Collection     $select
+     *
+     * @return array
+     */
+    public function serialize(EntityContract $entity, Collection $select = null)
+    {
+        $bag = new Bag($entity->toArray());
+
+        if ($select) {
+            $bag = $bag->only($select->toArray());
+        }
+
+        $bag = $bag->only($this->getManager()->authorizer->getAuthorizedAttributes(Tokens::PERMISSION_SHOW, $entity)->keys()->toArray());
+
+        return $bag;
     }
 }
