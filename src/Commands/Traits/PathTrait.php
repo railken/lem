907de9534c201cc80686a799a2e8c51e8675d135
railken <railken@protonmail.ch>
2018-09-11 22:04:16 +0200

    update path generation managers

--- src/Commands/Traits/PathTrait.php
+++ src/Commands/Traits/PathTrait.php
@@ -6,10 +6,6 @@ trait PathTrait
 {
     public function getAbsolutePathByParameter($path)
     {
-        if ($path[0] !== '/') {
-            $path = base_path($path);
-        }
-
-        return $path;
+        return getcwd()."/".$path;
     }
 }
