325bf36e59c90514479db72e8330334d4e3f6d65
railken <railken@protonmail.ch>
2018-09-23 06:09:25 +0200

    cleanup

--- tests/Generated/Foo/FooSerializer.php
+++ tests/Generated/Foo/FooSerializer.php
@@ -3,10 +3,8 @@
 namespace Railken\Lem\Tests\Generated\Foo;
 
 use Illuminate\Support\Collection;
-use Railken\Bag;
 use Railken\Lem\Contracts\EntityContract;
 use Railken\Lem\Serializer;
-use Railken\Lem\Tokens;
 
 class FooSerializer extends Serializer
 {
@@ -14,7 +12,7 @@ class FooSerializer extends Serializer
      * Serialize entity.
      *
      * @param \Railken\Lem\Contracts\EntityContract $entity
-     * @param \Illuminate\Support\Collection $select
+     * @param \Illuminate\Support\Collection        $select
      *
      * @return \Railken\Bag
      */
