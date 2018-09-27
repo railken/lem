3435394d3e25fb189e6b9d78577f94dcd98220ff
railken <railken@protonmail.ch>
2018-09-27 02:23:10 +0200

    cleanup

--- src/Attributes/IdAttribute.php
+++ src/Attributes/IdAttribute.php
@@ -5,16 +5,15 @@ namespace Railken\Lem\Attributes;
 class IdAttribute extends BaseAttribute
 {
     /**
-     * Name attribute.
-     *
-     * @var string
-     */
-    protected $name = 'id';
-
-    /**
      * Describe this attribute.
      *
      * @var string
      */
     public $comment = 'Indentify the entity';
+    /**
+     * Name attribute.
+     *
+     * @var string
+     */
+    protected $name = 'id';
 }
