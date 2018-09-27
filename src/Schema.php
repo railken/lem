6222f88ac9fa8345dd0e4c2484d7b01d627d07ea
railken <railken@protonmail.ch>
2018-09-28 00:57:12 +0200

    helpers methods for schema

--- src/Schema.php
+++ src/Schema.php
@@ -6,4 +6,17 @@ use Railken\Lem\Contracts\SchemaContract;
 
 abstract class Schema implements SchemaContract
 {
+    public function getFillableAttributes()
+    {
+        return array_filter($this->getAttributes(), function ($attribute) {
+            return $attribute->getFillable();
+        });
+    }
+
+    public function getNameFillableAttributes()
+    {
+        return array_map(function ($attribute) {
+            return $attribute->getName();
+        }, $this->getFillableAttributes());
+    }
 }
