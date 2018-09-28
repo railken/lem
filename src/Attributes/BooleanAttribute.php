0b838b4778a21c96f8622103bd318e140a1c4ab3
railken <railken@protonmail.ch>
2018-09-28 02:38:25 +0200

    add BooleanAttribute

---
+++ src/Attributes/BooleanAttribute.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Railken\Lem\Attributes;
+
+use Railken\Lem\Contracts\EntityContract;
+
+class BooleanAttribute extends BaseAttribute
+{
+    /**
+     * Is a value valid ?
+     *
+     * @param EntityContract $entity
+     * @param mixed          $value
+     *
+     * @return bool
+     */
+    public function valid(EntityContract $entity, $value)
+    {
+        return 1 === $value || 0 === $value || true === $value || false === $value;
+    }
+}
