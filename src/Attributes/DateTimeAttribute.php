378f90438e7364c21a38dd8db982577302803c05
railken <railken@protonmail.ch>
2018-10-01 23:35:50 +0200

    DateTime default fillable=true

--- src/Attributes/DateTimeAttribute.php
+++ src/Attributes/DateTimeAttribute.php
@@ -4,4 +4,10 @@ namespace Railken\Lem\Attributes;
 
 class DateTimeAttribute extends BaseAttribute
 {
+    /**
+     * Is the attribute fillable.
+     *
+     * @var bool
+     */
+    protected $fillable = true;
 }
