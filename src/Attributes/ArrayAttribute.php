f47677ccfe197907b447907c1e0d5baed299beb6
railken <railken@protonmail.ch>
2018-10-14 20:41:39 +0200

    add ArrayAttribute

---
+++ src/Attributes/ArrayAttribute.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Railken\Lem\Attributes;
+
+use Railken\Lem\Contracts\EntityContract;
+
+class ArrayAttribute extends TextAttribute
+{
+    /**
+     * Is a value valid ?
+     *
+     * @param \Railken\Lem\Contracts\EntityContract $entity
+     * @param mixed                                 $value
+     *
+     * @return bool
+     */
+    public function valid(EntityContract $entity, $value)
+    {
+        return is_array($value);
+    }
+
+    /**
+     * Retrieve default value.
+     *
+     * @param \Railken\Lem\Contracts\EntityContract $entity
+     *
+     * @return mixed
+     */
+    public function getDefault(EntityContract $entity)
+    {
+        return [];
+    }
+}
