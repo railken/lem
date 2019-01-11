941eedc399cbd4658122f4f81937039796a7b0a1
railken <railken@protonmail.ch>
2019-01-11 10:11:30 +0100

    Add conversion object=>yaml in YamlAttribute

--- src/Attributes/YamlAttribute.php
+++ src/Attributes/YamlAttribute.php
@@ -2,6 +2,7 @@
 
 namespace Railken\Lem\Attributes;
 
+use Railken\Bag;
 use Railken\Lem\Contracts\EntityContract;
 use Symfony\Component\Yaml\Exception\ParseException;
 use Symfony\Component\Yaml\Yaml;
@@ -25,5 +26,22 @@ class YamlAttribute extends TextAttribute
         }
 
         return true;
+    }
+
+    /**
+     * Update entity value.
+     *
+     * @param \Railken\Lem\Contracts\EntityContract $entity
+     * @param \Railken\Bag                          $parameters
+     *
+     * @return Collection
+     */
+    public function update(EntityContract $entity, Bag $parameters)
+    {
+        if (is_object($parameters->get($this->name)) || is_array($parameters->get($this->name))) {
+            $parameters->set($this->name, Yaml::dump($parameters));
+        }
+
+        return parent::update($entity, $parameters);
     }
 }
