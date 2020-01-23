43df6ff5951b53bb4b5853338e05c2aceac32b29
Railken <railken@protonmail.ch>
2020-01-23 03:38:15 +0100

    max length

--- src/Attributes/HtmlAttribute.php
+++ src/Attributes/HtmlAttribute.php
@@ -4,4 +4,10 @@ namespace Railken\Lem\Attributes;
 
 class HtmlAttribute extends LongTextAttribute
 {
+    /**
+     * MaxLength.
+     *
+     * @var int
+     */
+    protected $maxLength = 4000000;
 }
